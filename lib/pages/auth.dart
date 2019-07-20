d79b4b46f434e01fabd981eb72885a8f5719ae8a
sohagmahin <sohagmahin@gmail.com>
2019-07-20 23:00:34 +0600

    Added gestureDetector for disable keyboard when we press body of auth page

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -66,36 +66,41 @@ class _auth extends State<auth> {
     final double deviceWidth = MediaQuery.of(context).size.width;
     /*(deviceWidth * 0.95 ) means set Container width 95% of our screen . device width compare 768.
     because 768 is break point of mobile screen. In range of high mainly use for table device*/
-    final double targetWidth= deviceWidth > 768.0 ? 500 : deviceWidth * 0.95;
+    final double targetWidth = deviceWidth > 768.0 ? 500 : deviceWidth * 0.95;
     return Scaffold(
         appBar: AppBar(title: Text('LOGIN PANNEL')),
-        body: Container(
-            decoration: BoxDecoration(image: _buildBackgroundImage()),
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-                child: SingleChildScrollView(
-              child: Container(
-                width: targetWidth,
-                child: Column(
-                  children: <Widget>[
-                    _buildEmailTextField(),
-                    SizedBox(height: 10.0),
-                    _buildPasswordTextField(),
-                    _buildAcceptSwitch(),
-                    SizedBox(height: 10),
-                    RaisedButton(
-                      child: Text('Login'),
-                     color: Theme.of(context).primaryColor,
-                      textColor: Colors.white,
-                      onPressed: () => {
-                        print(_emailValue),
-                        print(_passwordValue),
-                        Navigator.pushReplacementNamed(context, '/products'),
-                      },
+        body: GestureDetector(
+            onTap: () {
+              FocusScope.of(context).requestFocus(FocusNode());
+            },
+            child: Container(
+                decoration: BoxDecoration(image: _buildBackgroundImage()),
+                padding: EdgeInsets.all(10.0),
+                child: Center(
+                    child: SingleChildScrollView(
+                  child: Container(
+                    width: targetWidth,
+                    child: Column(
+                      children: <Widget>[
+                        _buildEmailTextField(),
+                        SizedBox(height: 10.0),
+                        _buildPasswordTextField(),
+                        _buildAcceptSwitch(),
+                        SizedBox(height: 10),
+                        RaisedButton(
+                          child: Text('Login'),
+                          color: Theme.of(context).primaryColor,
+                          textColor: Colors.white,
+                          onPressed: () => {
+                            print(_emailValue),
+                            print(_passwordValue),
+                            Navigator.pushReplacementNamed(
+                                context, '/products'),
+                          },
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
-            ))));
+                  ),
+                )))));
   }
 }
