c4f519adc88386fde8e20cf904d0547bbf5e791c
sohagmahin <sohagmahin@gmail.com>
2019-07-26 00:11:30 +0600

    Improved UserInterface of my products ListTile

--- lib/pages/productList.dart
+++ lib/pages/productList.dart
@@ -4,27 +4,35 @@ import './product_edit.dart';
 class productListPage extends StatelessWidget {
   List<Map<String, dynamic>> _products;
   final Function updateProduct;
-  productListPage(this._products,this.updateProduct);
+  productListPage(this._products, this.updateProduct);
 
   @override
   Widget build(BuildContext context) {
     // return Center(child: Image.asset(_products[0]['image']),);
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          leading: Image.asset(_products[index]['image']),
-          title: Text(_products[index]['title']),
-          trailing: IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (BuildContext context) => productEdit(
-                        product: _products[index],
-                        updateProduct: updateProduct,
-                        productIndex: index,
-                      )));
-            },
-          ),
+        return Column(
+          children: <Widget>[
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(_products[index]['image']),
+              ),
+              subtitle: Text('\$${_products[index]['price']}'),
+              title: Text(_products[index]['title']),
+              trailing: IconButton(
+                icon: Icon(Icons.edit),
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (BuildContext context) => productEdit(
+                            product: _products[index],
+                            updateProduct: updateProduct,
+                            productIndex: index,
+                          )));
+                },
+              ),
+            ),
+            Divider()
+          ],
         );
       },
       itemCount: _products.length,
